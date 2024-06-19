5c2e5aa34451cec2211ee4e375db096ba05fdbd7
priya2413 <haripriyagh@gmail.com>
2024-06-19 13:38:08 +0530

    files added

---
+++ my-project/config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '7e83da8406247e09ce51f9076190bfa9'),
+    },
+  },
+});
---
+++ my-project/extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '9298197c-340f-489a-b5c8-50747275a026'
+};
